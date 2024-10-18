bac3c9ed9ab2eff3cf93290859e2c181a9828c51
willydekeyser <ma074172@skynet.be>
2024-10-18 15:34:09 +0200

    Init

---
+++ Java_1.java
@@ -0,0 +1,39 @@
+public class Java_1 {
+    public static void main(String[] args) {
+        
+        System.out.println("JAVA - primitive data types in Java");
+        
+            // 1 byte / -128 to 127
+        byte myByte = 12;
+        System.out.println("myByte = " + myByte);
+
+            // 2 bytes / -32.768 to 32.767
+        short myShort = 12;
+        System.out.println("myShort = " + myShort);
+
+            // 4 bytes / -2.147.183.648 to 2.147.483.647
+        int myInt = 12;
+        System.out.println("myInt = " + myInt);
+        
+            // 8 bytes / -9.223.372.036.854.775.808 to 9.223.372.036.854.775.807
+        long myLong = 12l;
+        System.out.println("myLong = " + myLong);
+
+            // 4 bytes / 6 decimal digits
+        float myFloat = 12f;
+        System.out.println("myFloat = " + myFloat);
+
+            // 8 bytes / 15 decimal digits 
+        double myDouble = 12d;
+        System.out.println("myDouble = " + myDouble);
+        
+            // 1 bit / true or false
+        boolean myBoolean = true;
+        System.out.println("myBoolean = " + myBoolean);
+
+            // 2 bytes / single character
+        char myChar = 'f';
+        System.out.println("myChar = " + myChar);
+
+    }
+}
